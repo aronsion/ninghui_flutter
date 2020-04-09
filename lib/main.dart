6c7298055d1fbe44124093cc801cb84e501138aa
Cappuccino <arons.hui@gmail.com>
2020-04-09 14:05:30 +0800

    其他列表项数据处理

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      debugShowCheckedModeBanner: false,
+      home: GridViewPage(),
       theme: ThemeData(
         primaryColor: Colors.yellow
       ),
@@ -16,8 +17,117 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Home extends StatelessWidget{
 
+
+class Horizontal extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('基础列表'),
+      ),
+      body: Container(
+        margin: EdgeInsets.symmetric(vertical: 20.0),
+        height: 200.0,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            Container(
+              width: 160.0,
+              color: Colors.lightBlue,
+            ),
+            Container(
+              width: 160.0,
+              color: Colors.lightBlue,
+            ),
+            Container(
+              width: 160.0,
+              color: Colors.green,
+              child: Column(
+                children: <Widget>[
+                  Text('水平',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 36.0),),
+                  Text('列表',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 36.0),),
+                  Icon(Icons.alarm)
+                ],
+              ),
+            ),
+            Container(
+              width: 160.0,
+              color: Colors.amberAccent,
+            ),
+            Container(
+              width: 160.0,
+              color: Colors.cyan,
+            ),
+            Container(
+              width: 160.0,
+              color: Colors.pinkAccent,
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
+
+class GridViewPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    final titlew ='网格列表';
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(titlew),
+      ),
+      body: GridView.count(
+        crossAxisCount: 3,
+        primary: true,
+        padding: EdgeInsets.all(8.0),
+        crossAxisSpacing: 10.0,
+        children: <Widget>[
+          Image.network(postList[0].imageUrl),
+          Image.network(postList[1].imageUrl),
+          Image.network(postList[2].imageUrl),
+          Image.network(postList[3].imageUrl),
+          Image.network(postList[4].imageUrl),
+          Image.network(postList[5].imageUrl),
+          Image.network(postList[6].imageUrl),
+          Image.network(postList[7].imageUrl),
+          Image.network(postList[8].imageUrl),
+          Image.network(postList[0].imageUrl),
+          Image.network(postList[1].imageUrl),
+          Image.network(postList[2].imageUrl),
+          Image.network(postList[3].imageUrl),
+          Image.network(postList[4].imageUrl),
+          Image.network(postList[5].imageUrl),
+          Image.network(postList[6].imageUrl),
+          Image.network(postList[7].imageUrl),
+          Image.network(postList[8].imageUrl),
+          Image.network(postList[0].imageUrl),
+          Image.network(postList[1].imageUrl),
+          Image.network(postList[2].imageUrl),
+          Image.network(postList[3].imageUrl),
+          Image.network(postList[4].imageUrl),
+          Image.network(postList[5].imageUrl),
+          Image.network(postList[6].imageUrl),
+          Image.network(postList[7].imageUrl),
+          Image.network(postList[8].imageUrl),
+          Image.network(postList[0].imageUrl),
+          Image.network(postList[1].imageUrl),
+          Image.network(postList[2].imageUrl),
+          Image.network(postList[3].imageUrl),
+          Image.network(postList[4].imageUrl),
+          Image.network(postList[5].imageUrl),
+          Image.network(postList[6].imageUrl),
+          Image.network(postList[7].imageUrl),
+          Image.network(postList[8].imageUrl),
+        ],
+      ),
+    );
+  }
+
+}
+
+class Home extends StatelessWidget{
   Widget _listItemBuilder(BuildContext context,int index){
     return Container(
       color: Colors.white,
@@ -38,7 +148,6 @@ class Home extends StatelessWidget{
         ],
       ),
     );
-
   }
 
   @override
