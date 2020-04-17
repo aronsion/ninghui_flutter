60bbef2cef2e21cf46cbd16df5a8f81e671f7fc1
Cappuccino <arons.hui@gmail.com>
2020-04-17 22:22:14 +0800

    增加列表数据

---
+++ lib/pages/ShoppingPage.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:ninghui_flutter/pages/shopping/StepperPage.dart';
+
+class ShoppingPage extends StatefulWidget {
+  @override
+  _ShoppingPageState createState() => _ShoppingPageState();
+}
+
+class _ShoppingPageState extends State<ShoppingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: ListView(
+        children: <Widget>[
+          ListItem(title: '',page: StepperPage(),)
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+class ListItem extends StatelessWidget {
+  final String title;
+  final Widget page;
+
+
+  ListItem({this.title, this.page});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      onTap: (){
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => page),
+        );
+      },
+    );
+  }
+}
---
+++ lib/pages/shopping/StepperPage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class StepperPage extends StatefulWidget {
+  @override
+  _StepperPageState createState() => _StepperPageState();
+}
+
+class _StepperPageState extends State<StepperPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
