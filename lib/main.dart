761f229c3a538f9da7ff6c67ab6fe966e5b9263e
Cappuccino <arons.hui@gmail.com>
2020-04-18 23:10:38 +0800

    title名称

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: '淘宝网',
       theme: ThemeData(
         primarySwatch: Colors.deepOrange,
       ),
