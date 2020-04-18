91fd58557ab056fcfb2be3ad1ac6666f1a67e216
Cappuccino <arons.hui@gmail.com>
2020-04-18 23:18:20 +0800

    我的页面完善

--- lib/pages/MyPage.dart
+++ lib/pages/MyPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ninghui_flutter/model/PostData.dart';
 
 class MyPage extends StatefulWidget {
   @override
@@ -8,6 +9,58 @@ class MyPage extends StatefulWidget {
 class _MyPageState extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Container(
+          padding: EdgeInsets.all(16.0),
+          child: ListView(
+            children: postList.map((post) {
+              return Card(
+                child: Column(
+                  children: <Widget>[
+                    AspectRatio(
+                      aspectRatio: 16/9,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(4.0),
+                          topRight: Radius.circular(4.0),
+                        ),
+                        child: Image.network(
+                          post.imageUrl,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(post.imageUrl),
+                      ),
+                      title: Text(post.title),
+                      subtitle: Text(post.author),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text(post.description, maxLines: 2, overflow: TextOverflow.ellipsis,),
+                    ),
+                    ButtonTheme.bar(
+                      child: ButtonBar(
+                        children: <Widget>[
+                          FlatButton(
+                            child: Text('Like'.toUpperCase()),
+                            onPressed: () {},
+                          ),
+                          FlatButton(
+                            child: Text('Read'.toUpperCase()),
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
+          ),
+        )
+    );
   }
 }
